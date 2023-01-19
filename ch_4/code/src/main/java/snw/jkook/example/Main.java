02c2d56dec4e9618db3e1cd9d47d5968d6985c7b
ZX夏夜之风 <snwcreations@qq.com>
2023-01-19 11:40:27 +0800

    Update Chapter 4.

--- ch_4/code/src/main/java/snw/jkook/example/Main.java
+++ ch_4/code/src/main/java/snw/jkook/example/Main.java
@@ -3,9 +3,6 @@ package snw.jkook.example;
 import snw.jkook.JKook;
 import snw.jkook.command.JKookCommand;
 import snw.jkook.entity.CustomEmoji;
-import snw.jkook.entity.Guild;
-import snw.jkook.entity.channel.TextChannel;
-import snw.jkook.message.TextChannelMessage;
 import snw.jkook.message.component.MarkdownComponent;
 import snw.jkook.plugin.BasePlugin;
 
@@ -29,21 +26,6 @@ public class Main extends BasePlugin {
                     replyContent.append("你的名字: ").append(name).append("\n");
                     replyContent.append("你的用户 ID: ").append(userId).append("\n");
                     replyContent.append("你的完整用户名称: ").append(fullName);
-
-                    if (message instanceof TextChannelMessage) { // if this command was executed in a channel
-                        replyContent.append("\n"); // ignore this please, just for better look
-
-                        TextChannel channel = ((TextChannelMessage) message).getChannel();
-                        Guild guild = channel.getGuild();
-                        String guildName = guild.getName();
-                        String guildId = guild.getId();
-                        String channelName = channel.getName();
-                        String channelId = channel.getId();
-                        replyContent.append("此消息所在的服务器的名称: ").append(guildName).append("\n");
-                        replyContent.append("此消息所在的服务器的 ID: ").append(guildId).append("\n");
-                        replyContent.append("此消息所在的频道的名称: ").append(channelName).append("\n");
-                        replyContent.append("此消息所在的频道的 ID: ").append(channelId).append("\n");
-                    }
 
                     message.reply(new MarkdownComponent(replyContent.toString()));
                 })
